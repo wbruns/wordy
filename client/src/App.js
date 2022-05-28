5db6eeb778737489d641611b0b2ec854041a3128
Mouse0226 <mouse1987.ml@gmail.com>
2022-05-28 10:41:21 -0500

    Initializing client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import NoMatch from "./pages/NoMatch";
-import Profile from "./pages/Profile";
-import GamePage from "./pages/GamePage";
-import About from "./pages/About";
-
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import React from 'react';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>It's a page!</div>
-      </Router>
-    </ApolloProvider>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
