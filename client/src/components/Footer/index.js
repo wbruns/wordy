4091cb050f1381fa629dc39dcb795a8410ea3a25
Mouse0226 <mouse1987.ml@gmail.com>
2022-05-30 10:58:00 -0500

    Centering Text

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <div>&copy;2022 - Built by Shannon S, Michael L and Will B</div>
+            <div className="footer-text">&copy;2022 - Built by Shannon S, Michael L and Will B</div>
         </footer>
     );
 };
