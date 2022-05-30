a083d2e41abf86f79c5a3a90a709f472d3f2b288
SSto <95592688+sstover25@users.noreply.github.com>
2022-05-29 21:20:13 -0500

    algos for Game logic added

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,64 +1,74 @@
-import React, { useState } from 'react';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import Auth from "../utils/auth";
 
-        // const { loading, data } = useQuery(QUERY_GAME, {
-        //     variables: { game_username: username }
-        // });
+// const { loading, data } = useQuery(QUERY_GAME, {
+//     variables: { game_username: username }
+// });
 
 const Game = () => {
-    const [formState, setFormState] = useState({guess: ''});
+  const [formState, setFormState] = useState({ guess: "" });
 
-    const username = Auth.getProfile().data.username;
+  const username = Auth.getProfile().data.username;
+  console.log(username);
 
-    console.log(username);
-    
+  const imageArray = [
+    "../assets/Image_1.jpg",
+    "../assets/Image_2.jpg",
+    "../assets/Image_3.jpg",
+    "../assets/Image_4.jpg",
+    "../assets/Image_5.jpg",
+    "../assets/Image_6.jpg",
+    "../assets/Image_7.jpg",
+  ];
 
-    const handleChange = (event) => {
-        event.preventDefault();
-        const { name, value } = event.target;
+  let imageIndex = 0;
 
-        setFormState({
-        ...formState,
-        [name]: value,
-        });
-    };
-    
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
 
-        formState.guess = formState.guess.toUpperCase();
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-        console.log(formState.guess);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-    };
+    formState.guess = formState.guess.toUpperCase();
 
-    return (
-        <main>
-            <div>
-                GAME TIME
-            </div>
-            <div>
-                <img src={require("../assets/Image_1.jpg")} alt={"Carlie Anglemire"}/>
-            </div>
-            <div>
-                <form onSubmit={handleFormSubmit}>
-                    <label htmlFor='guess'>Guess:</label>
-                    <input
-                        placeholder="Enter a letter"
-                        name='guess' type='guess'
-                        maxLength={1}
-                        required pattern="^[A-Za-z]+$"
-                        value={formState.guess}
-                        onChange={handleChange} />
-                    <button type='submit'>
-                        Submit
-                    </button>
-                </form>
-                
-            </div>
-        </main>
-        
-    );
+    console.log(formState.guess);
+  };
+
+  return (
+    <main>
+      <div>GAME TIME</div>
+      <div>
+        <img
+          id="hangman"
+          src={require(imageArray[imageIndex])}
+          alt={"Carlie Anglemire"}
+        />
+      </div>
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="guess">Guess:</label>
+          <input
+            placeholder="Enter a letter"
+            name="guess"
+            type="guess"
+            maxLength={1}
+            required
+            pattern="^[A-Za-z]+$"
+            value={formState.guess}
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Game;
---
+++ client/src/pages/TEMP_ALGOS.js
@@ -0,0 +1,34 @@
+// // This checks to see if at least one letter matches the user's input. If it does, then it returns true.
+// function checkLetters(array, letter) {
+//   return array.some(function (solution) {
+//     return letter === solution;
+//   });
+// }
+
+// checkLetters(todays_word, input);
+
+// if (checkLetters === true) {
+//   // If letter matches one from "todays_word", then it will push that letter to the "current_word" array at that index
+//   for (i = 0; i < todays_word.length; i++) {
+//     if (input === todays_word[i]) {
+//       current_word.splice(i, 1, input);
+//     }
+//   }
+
+//   // Push the letter into the correct_letters_guessed array
+//   correct_letters_guessed.push(input);
+
+//   if (todays_word === current_word) {
+//     // End the game - WIN
+//     // have a modal pop up?
+//   }
+// } else {
+//   // Push the letter into the incorrect_letters_guessed array
+//   incorrect_letters_guessed.push(input);
+//   // Change the image
+//   imageIndex = imageIndex + 1;
+//   if (incorrect_letters_guessed.length === 7) {
+//     // End the game - LOSE
+//     // have a modal pop up?
+//   }
+// }
