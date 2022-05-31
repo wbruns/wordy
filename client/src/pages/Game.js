28a871db193afc49002c21262774c94834b73d09
SSto <95592688+sstover25@users.noreply.github.com>
2022-05-31 13:30:23 -0500

    added game_finished logic and comments into Game.js

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -8,36 +8,39 @@ import { Navigate } from "react-router-dom";
 
 const Game = () => {
   const [formState, setFormState] = useState({ guess: "" });
-  const [updateGame] = useMutation(UPDATE_GAME);
   const username = Auth.getProfile().data.username;
-  const [updateStats] = useMutation(UPDATE_STATS, { variables: { stats_username: username } });
-  const { data: userStats } = useQuery(QUERY_STATS, { variables: { stats_username: username } });
 
+  // Database calls
+  const [updateGame] = useMutation(UPDATE_GAME);
+  const [updateStats] = useMutation(UPDATE_STATS, {
+    variables: { stats_username: username },
+  });
+  const { data: userStats } = useQuery(QUERY_STATS, {
+    variables: { stats_username: username },
+  });
   const { loading, data: gameData } = useQuery(QUERY_GAME, {
     variables: { game_username: username },
   });
 
+  // Variables for Stats db calls
   let games_played = userStats?.stats.games_played;
   let games_won = userStats?.stats.games_won;
   let current_streak = userStats?.stats.current_streak;
 
   if (gameData) {
-    
-
+    // If the user has guessed incorrectly 6 times, the game ends and the user loses
     if (gameData.game.incorrect_letters_guessed.length >= 6) {
-      // End the game - LOSE
-      // have a modal pop up?
-      console.log(gameData);
-
+      // Update the stats to reset the current_streak and update the games_played by 1
       updateStats({
         variables: {
-            stats_username: username,
-            games_played: games_played + 1,
-            games_won: games_won,
-            current_streak: 0
-        }
+          stats_username: username,
+          games_played: games_played + 1,
+          games_won: games_won,
+          current_streak: 0,
+        },
       });
 
+      // Update the game to show game_finished as true
       updateGame({
         variables: {
           game_username: username,
@@ -46,14 +49,19 @@ const Game = () => {
           todays_word: gameData.game.todays_word,
           incorrect_letters_guessed: gameData.game.incorrect_letters_guessed,
           game_date: gameData.game.game_date,
-          game_finished: true
+          game_finished: true,
         },
       });
 
-      return <Navigate to="/profile" />
+      // Automatically route the user to profile when the game is done and has been lost
+      return <Navigate to="/profile" />;
+    } else if (gameData.game.game_finished === true) {
+      // Automatically route the user to profile when the game is done and has been won
+      return <Navigate to="/profile" />;
     }
   }
 
+  // Images of the hangman
   const imageArray = [
     "Image_1.jpg",
     "Image_2.jpg",
@@ -63,8 +71,6 @@ const Game = () => {
     "Image_6.jpg",
     "Image_7.jpg",
   ];
-
-  
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -78,8 +84,8 @@ const Game = () => {
 
   const handleFormSubmit = async (event) => {
     // event.preventDefault();
-    // debugger;
 
+    // Variables for Game db calls
     const correct_letters_guessed = [...gameData.game.correct_letters_guessed];
     const incorrect_letters_guessed = [
       ...gameData.game.incorrect_letters_guessed,
@@ -87,28 +93,22 @@ const Game = () => {
     const todays_word = [...gameData.game.todays_word];
     const current_word = [...gameData.game.current_word];
 
-    console.log(incorrect_letters_guessed);
-    console.log(correct_letters_guessed);
-    console.log(todays_word);
-    console.log(current_word);
-
+    // Automatically change user input to a capitalized letter
     formState.guess = formState.guess.toUpperCase();
 
     let guess = formState.guess;
 
-    console.log(guess);
-
+    // Function to check if the user's guess is already in the arrays incorrect_letters_guessed and correct_letters_guessed
     let incorrectCheck = checkLetters(incorrect_letters_guessed, guess);
-
     let correctCheck = checkLetters(correct_letters_guessed, guess);
 
+    // If it's not in either array, continue on to other checks. Otherwise, return message about duplicate guess.
     if (correctCheck || incorrectCheck) {
-      // if it's not in either, do the other checks
-
       alert("You've already guessed this, idiot.");
       return;
     }
 
+    // Function to check if user's guess matches at least one letter in todays_word
     let letterCheck = checkLetters(todays_word, guess);
 
     if (letterCheck) {
@@ -122,8 +122,8 @@ const Game = () => {
 
       // Push the letter into the correct_letters_guessed array
       correct_letters_guessed.push(guess);
-      console.log(correct_letters_guessed);
-      // update game call
+
+      // Update game call with new current_word and correct_letters_guessed arrays
       updateGame({
         variables: {
           game_username: username,
@@ -132,22 +132,23 @@ const Game = () => {
           todays_word: gameData.game.todays_word,
           incorrect_letters_guessed: gameData.game.incorrect_letters_guessed,
           game_date: gameData.game.game_date,
-          game_finished: gameData.game.game_finished
+          game_finished: gameData.game.game_finished,
         },
       });
 
-      if (!current_word.includes('-')) {
-        // End the game - WIN!!
-        // have a modal pop up?
+      // If all - has been replaced in the current_word, then the user has won
+      if (!current_word.includes("-")) {
+        // Update the user's stats to increase all fields by 1 after win
         updateStats({
           variables: {
-              stats_username: username,
-              games_played: games_played + 1,
-              games_won: games_won + 1,
-              current_streak: current_streak + 1
-          }
+            stats_username: username,
+            games_played: games_played + 1,
+            games_won: games_won + 1,
+            current_streak: current_streak + 1,
+          },
         });
 
+        // Update the game with the new current_word and correct_letters_guessed arrays and change game_finished to true
         updateGame({
           variables: {
             game_username: username,
@@ -156,19 +157,18 @@ const Game = () => {
             todays_word: gameData.game.todays_word,
             incorrect_letters_guessed: gameData.game.incorrect_letters_guessed,
             game_date: gameData.game.game_date,
-            game_finished: true
+            game_finished: true,
           },
         });
 
-        return <Navigate to="/profile" />
+        // Automatically route the user to profile when the game is done and has been won
+        return <Navigate to="/profile" />;
       }
-
     } else {
       // Push the letter into the incorrect_letters_guessed array
       incorrect_letters_guessed.push(guess);
-      console.log(incorrect_letters_guessed);
 
-      // update game call
+      // Update the game with the new incorrect_letters_guessed array
       updateGame({
         variables: {
           game_username: username,
@@ -177,18 +177,14 @@ const Game = () => {
           correct_letters_guessed: gameData.game.correct_letters_guessed,
           todays_word: gameData.game.todays_word,
           game_date: gameData.game.game_date,
-          game_finished: gameData.game.game_finished
+          game_finished: gameData.game.game_finished,
         },
       });
     }
   };
 
-  //src={require(`../assets/${imageArray[imageIndex]}`)}
-
   if (loading) {
-    return (
-      <div>loading</div>
-    )
+    return <div>Loading...</div>;
   }
 
   return (
@@ -197,8 +193,10 @@ const Game = () => {
       <div>
         <img
           id="hangman"
-          src={require(`../assets/${imageArray[gameData.game.incorrect_letters_guessed.length || 0]}`)}
-          alt="Carlie Anglemire"
+          src={require(`../assets/${
+            imageArray[gameData.game.incorrect_letters_guessed.length || 0]
+          }`)}
+          alt="The hangman person"
         />
       </div>
       <div>{gameData.game.correct_letters_guessed}</div>
